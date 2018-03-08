8d3d6ded9d6c516b6bd370d4ccafa81a508358cb
Aravind Balan <aravind.balan@nasa.gov>
2018-03-08 10:58:39 -0500

    to run in my account

--- onera-m6/fun3d-fv-l2/wall-distance.sh
+++ onera-m6/fun3d-fv-l2/wall-distance.sh
@@ -5,15 +5,9 @@ set -u # Treat unset variables as error
 
 . /usr/local/pkgs/modules/init/bash
 
-module use /u/shared/fun3d/fun3d_users/onering_modulefiles/linux-centos6-x86_64
+
 module purge
-module load Python_2.7.8
-module load cmake-3.9.4-gcc-6.2.0-e24o65d
 module load gcc_6.2.0
-module load intel_2017.2.174
-module load mpich-3.2-gcc-6.2.0-duvhaci
-module load ParaView_5.4.1
-module load gnuplot_4.6.3
 
 set -x # echo commands
 
@@ -22,7 +16,7 @@ dist=${ugrid%b8.ugrid}dist
 out=${ugrid%b8.ugrid}dist-out
 
 echo "3 4 5 6 7 8 9 10 11 12" |
-/misc/home2/mpark/gitlab/one-ring/parfait/build/utils/DistanceCalculator/DistanceCalculator $1 > ${out}
+/u/abalan/parfait/build/utils/DistanceCalculator/DistanceCalculator $1 > ${out}
 
 grep -v \# < distance.txt | cut -d' ' -f5 > ${dist}
 
