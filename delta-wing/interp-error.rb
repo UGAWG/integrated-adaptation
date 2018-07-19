8a23424544f90dc3b7bff17f5ae0e7bb5a6a9829
Mike Park <Mike.Park@NASA.gov>
2018-07-19 05:53:12 -0400

    calc

---
+++ delta-wing/interp-error.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+t=29
+root='Flow/delta'
+norm=2
+
+(1..(truth-1)).each do |i|
+  truth=sprintf("%s%02d",root,t)
+  candidate=sprintf("%s%02d",root,i)
+  comm = "ref_interp_test --error ${truth}.meshb ${truth}_volume.solb ${candidate}.meshb ${candidate}_volume.solb ${norm}"
+  system comm
+end
