cf2e205e342260cdc200fb59959755705c44d260
Mike Park <Mike.Park@NASA.Gov>
2018-07-19 05:58:18 -0400

    ruby-syntax

--- delta-wing/interp-error.rb
+++ delta-wing/interp-error.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
 
-t=29
+t=28
 root='Flow/delta'
 norm=2
 
 (1..(t-1)).each do |i|
   truth=sprintf("%s%02d",root,t)
   candidate=sprintf("%s%02d",root,i)
-  comm = "ref_interp_test --error ${truth}.meshb ${truth}_volume.solb ${candidate}.meshb ${candidate}_volume.solb ${norm}"
+  comm = "ref_interp_test --error #{truth}.meshb #{truth}_volume.solb #{candidate}.meshb #{candidate}_volume.solb #{norm}"
   system comm
 end
