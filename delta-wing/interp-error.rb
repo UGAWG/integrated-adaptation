51e22efa2839ac343820f0278686566f6053d5f5
Mike Park <Mike.Park@NASA.gov>
2018-07-19 05:53:54 -0400

    rename

--- delta-wing/interp-error.rb
+++ delta-wing/interp-error.rb
@@ -4,7 +4,7 @@ t=29
 root='Flow/delta'
 norm=2
 
-(1..(truth-1)).each do |i|
+(1..(t-1)).each do |i|
   truth=sprintf("%s%02d",root,t)
   candidate=sprintf("%s%02d",root,i)
   comm = "ref_interp_test --error ${truth}.meshb ${truth}_volume.solb ${candidate}.meshb ${candidate}_volume.solb ${norm}"
